91259bfb479bf73ed6d8b3734d833ebb0bb09a94
Francis Chen <fncis.chen@gmail.com>
2019-02-25 15:40:16 -0800

    updated movie component to allow for this.prop.match.params to connect each movie card

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -12,6 +12,7 @@ export default class Movie extends Component {
   componentDidMount() {
     // change this line to grab the id passed on the URL
     const id = this.props.match.params.id;
+    console.log(id);
     this.fetchMovie(id);
   }
 
