aa3aa9b986a3e049d62027e8dcbfa90916c3bc65
Francis Chen <fncis.chen@gmail.com>
2019-02-25 15:46:28 -0800

    allow user to click card to read more details about movie card

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -25,7 +25,9 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
+          <Link to={`/movies/${movie.id}`}>
           <MovieDetails key={movie.id} movie={movie} />
+          </Link>
         ))}
       </div>
     );
